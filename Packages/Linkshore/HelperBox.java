ac0498c02271103ef359d25e00a13e218c2bb045
EcjTn <ecjtaneo@gmail.com>
2025-07-17 20:35:25 +0800

    Add files via upload

---
+++ Packages/Linkshore/HelperBox.java
@@ -0,0 +1,12 @@
+package Linkshore;
+
+public class HelperBox {
+
+    public void serialize(){
+        System.out.println("On the way.");
+    }
+
+    public static void signKey(String key){
+        System.out.println(key + " Signed...");
+    }
+}
---
+++ Packages/Main.java
@@ -0,0 +1,12 @@
+import Linkshore.HelperBox;
+
+public class Main {
+    public static void main(String[] args) {
+
+        HelperBox helpbox = new HelperBox();
+
+        helpbox.serialize();
+        HelperBox.signKey("785-9340e-arfw-36t63"); //random
+
+    }
+}
